00ff82656416d4697c211187199a0ef20cebb167
hzmbeyond80 <zhiminhe@gmail.com>
2018-03-13 19:33:43 -0700

    Create zhimin-98.sol

---
+++ Lesson-1/assignment/zhimin-98.sol
@@ -0,0 +1,52 @@
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    uint salary = 1 ether;
+    address boss;
+    address employee;
+    uint constant payDuration = 10 seconds;
+    uint lastPayday = now;
+    
+    function Payroll() payable {
+        boss = msg.sender;
+    }
+    
+    function updateEmployee(address new_employee) {
+        if (msg.sender != boss) {
+            revert();
+        }
+        if (new_employee == 0x0) {
+            revert();
+        }
+        employee = new_employee;
+    }
+    
+    function updateSalary(uint new_salary) {
+        require (msg.sender == boss) ;
+        salary = new_salary * 1 ether ;
+    }
+    
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+    
+    function calculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+    
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0; 
+    }
+    
+    function getBalance() returns (uint) {
+        return this.balance;    
+    }
+    
+    function getPaid() payable {
+        require(msg.sender == employee);
+        uint nextPayDay = lastPayday + payDuration;
+        require(nextPayDay < now);
+        lastPayday = nextPayDay;
+        employee.transfer(salary);
+    }
+}
