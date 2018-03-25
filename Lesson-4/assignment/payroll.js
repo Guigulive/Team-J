7e611eca7950b92fe50b8a5982b633c1385bd14f
tanghua <tang_hua@163.com>
2018-03-25 19:32:47 +0800

    Update payroll.js

--- Lesson-4/assignment/payroll.js
+++ Lesson-4/assignment/payroll.js
@@ -18,7 +18,7 @@ contract('Payroll',function(accounts){
     it("Remove Employee Test...",function(){
         return Payroll.deployed().then(function(instance){
             payrollInstance = instance;
-            payrollInstance.addFund({from:accounts[0],value:web3.toWei('10000','ether')}); //删除员工前，增加一定数量的余额
+            payrollInstance.addFund({from:accounts[0],value:web3.toWei('1000','ether')}); //删除员工前，增加一定数量的余额
             return payrollInstance.removeEmployee(accounts[1]);
         }).then(function(){
             return payrollInstance.employees.call(accounts[1]);
